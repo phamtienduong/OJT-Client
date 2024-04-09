a6d1ea67a6b20be55deac29404a0a1abf4b1c4f6
ThienLang97 <lampr494@gmail.com>
2024-04-09 09:15:26 +0700

    hello

---
+++ src/Components/Header.jsx
@@ -0,0 +1,338 @@
+import { AiOutlineUser } from "react-icons/ai"; 
+import { AiOutlineShoppingCart } from "react-icons/ai"; 
+import { MdFavoriteBorder, MdSearch } from "react-icons/md"; 
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import {
+    ArrowPathIcon,
+    Bars3Icon,
+    ChartPieIcon,
+    CursorArrowRaysIcon,
+    FingerPrintIcon,
+    SquaresPlusIcon,
+    XMarkIcon,
+} from "@heroicons/react/24/outline";
+import "./Header.scss";
+import {
+    ChevronDownIcon,
+    PhoneIcon,
+    PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+import Translate from "./Translate";
+
+const products = [
+    {
+        name: "Analytics",
+        description: "Get a better understanding of your traffic",
+        href: "#",
+        icon: ChartPieIcon,
+    },
+    {
+        name: "Engagement",
+        description: "Speak directly to your customers",
+        href: "#",
+        icon: CursorArrowRaysIcon,
+    },
+    {
+        name: "Security",
+        description: "Your customers’ data will be safe and secure",
+        href: "#",
+        icon: FingerPrintIcon,
+    },
+    {
+        name: "Integrations",
+        description: "Connect with third-party tools",
+        href: "#",
+        icon: SquaresPlusIcon,
+    },
+    {
+        name: "Automations",
+        description: "Build strategic funnels that will convert",
+        href: "#",
+        icon: ArrowPathIcon,
+    },
+];
+const callsToAction = [
+    { name: "Watch demo", href: "#", icon: PlayCircleIcon },
+    { name: "Contact sales", href: "#", icon: PhoneIcon },
+];
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return (
+        <header className="bg-white backGroundCo sticky top-0 z-50">
+            <Translate></Translate>
+            <nav
+                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+                aria-label="Global"
+            >
+                <div className="flex items-center">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only"></span>
+                        <a
+                            href="#"
+                            style={{
+                                color: "black",
+                                fontSize: "40px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Exclusive
+                        </a>
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <Popover.Group className="hidden lg:flex lg:gap-x-12 items-center">
+                    <Popover className="relative">
+                        <Popover.Button className="flex items-center gap-x-1 text-m font-bold leading-6 text-black-800 mb-1 ml-5">
+                            Home
+                            <ChevronDownIcon
+                                className="h-5 w-5 flex-none text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </Popover.Button>
+
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 translate-y-1"
+                        >
+                            <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                                <div className="p-4">
+                                    {products.map((item) => (
+                                        <div
+                                            key={item.name}
+                                            className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                                        >
+                                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                                <item.icon
+                                                    className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                                                    aria-hidden="true"
+                                                />
+                                            </div>
+                                            <div className="flex-auto">
+                                                <a
+                                                    href={item.href}
+                                                    className="block font-semibold text-gray-900"
+                                                >
+                                                    {item.name}
+                                                    <span className="absolute inset-0" />
+                                                </a>
+                                                <p className="mt-1 text-gray-600">
+                                                    {item.description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                                    {callsToAction.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                                        >
+                                            <item.icon
+                                                className="h-5 w-5 flex-none text-gray-400"
+                                                aria-hidden="true"
+                                            />
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
+
+                    <a
+                        href="#"
+                        className="text-m font-bold leading-6 text-black-800 mb-1"
+                    >
+                        Contact
+                    </a>
+                    <a
+                        href="#"
+                        className="text-m font-bold leading-6 text-black-800 mb-1"
+                    >
+                        About
+                    </a>
+                    <a
+                        href="#"
+                        className="text-m font-bold leading-6 text-black-800 mb-1"
+                    >
+                        Sign up
+                    </a>
+
+                    <div className="relative">
+                        <input
+                            type="text"
+                            placeholder="Search here"
+                            className="pl-8 pr-3 py-1 rounded-lg border border-gray-300 text-s focus:outline-none focus:border-blue-500 "
+                        />
+                        <a
+                            href="#"
+                            className="absolute inset-y-0 right-0 flex pr-3 items-center"
+                            style={{ fontSize: "22px" }}
+                        >
+                            <MdSearch />
+                        </a>
+                    </div>
+                </Popover.Group>
+                <div className="hidden lg:flex lg:gap-x-12 items-center ml-5">
+                    <a
+                        href=""
+                        className="flex font-normal leading-6 text-black-800 mb-1"
+                        style={{ fontSize: "25px" }}
+                    >
+                        <MdFavoriteBorder />
+                    </a>
+                    <a
+                        href=""
+                        className="flex font-normal leading-6 text-black-800 mb-1"
+                        style={{ fontSize: "25px" }}
+                    >
+                        <AiOutlineShoppingCart />
+                    </a>
+                    <a
+                        href=""
+                        className="flex font-normal leading-6 text-black-800 mb-1"
+                        style={{ fontSize: "25px" }}
+                    >
+                        <AiOutlineUser />
+                    </a>
+                </div>
+                {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a
+                        href="#"
+                        className="text-sm font-bold leading-6 text-black-800"
+                    >
+                        <MdFavoriteBorder />{" "}
+                        <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div> */}
+                <div></div>
+            </nav>
+            <Dialog
+                as="div"
+                className="lg:hidden"
+                open={mobileMenuOpen}
+                onClose={setMobileMenuOpen}
+            >
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Exclusive</span>
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <Disclosure as="div" className="-mx-3">
+                                    {({ open }) => (
+                                        <>
+                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                                Home
+                                                <ChevronDownIcon
+                                                    className={classNames(
+                                                        open
+                                                            ? "rotate-180"
+                                                            : "",
+                                                        "h-5 w-5 flex-none"
+                                                    )}
+                                                    aria-hidden="true"
+                                                />
+                                            </Disclosure.Button>
+                                            <Disclosure.Panel className="mt-2 space-y-2">
+                                                {[
+                                                    ...products,
+                                                    ...callsToAction,
+                                                ].map((item) => (
+                                                    <Disclosure.Button
+                                                        key={item.name}
+                                                        as="a"
+                                                        href={item.href}
+                                                        className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                    >
+                                                        {item.name}
+                                                    </Disclosure.Button>
+                                                ))}
+                                            </Disclosure.Panel>
+                                        </>
+                                    )}
+                                </Disclosure>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Contact
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    About
+                                </a>
+
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Wishlist
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Cart
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Account
+                                </a>
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+            <hr className="w-full"/>
+        </header>
+        
+    );
+}
