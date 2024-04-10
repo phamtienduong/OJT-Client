418c0fe41fe7bf1223d83f0f81683ff83aef95bf
ThienLang97 <lampr494@gmail.com>
2024-04-10 08:19:49 +0700

    Lam-10-4

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import Error from './pages/Error/Error'
 import { Route, Routes } from 'react-router-dom'
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
+//
+import { TECollapse, TERipple } from "tw-elements-react";
+import Favorite from './pages/Favorite/Favorite'
+import Products from './pages/Products/Products'
 export default function App() {
+  
   return (
+    
     <Routes>
      <Route path="/" element={<HomePage />}></Route>
      <Route path="/error" element={<Error />}></Route>
      <Route path="/login" element={<Login />}></Route>
      <Route path="/register" element={<Register />}></Route>
+     <Route path="/favor" element={<Favorite />}></Route>
+     <Route path="/products" element={<Products />}></Route>
     </Routes>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,8 +11,13 @@ import {
     FingerPrintIcon,
     SquaresPlusIcon,
     XMarkIcon,
+    UserIcon,
+    ArchiveBoxIcon,
+    StarIcon,
+    ArrowLeftOnRectangleIcon
 } from "@heroicons/react/24/outline";
 import "./Header.scss";
+
 import {
     ChevronDownIcon,
     PhoneIcon,
@@ -56,10 +61,24 @@ const callsToAction = [
     { name: "Watch demo", href: "#", icon: PlayCircleIcon },
     { name: "Contact sales", href: "#", icon: PhoneIcon },
 ];
+const products2 = [
+    { name: "My Account", href: "#", icon: UserIcon },
+    { name: "My Orders", href: "#", icon: ArchiveBoxIcon },
+    { name: "My Reviews", href: "#", icon: StarIcon },
+    { name: "Logout", href: "#", icon: ArrowLeftOnRectangleIcon },
+];
 
+const categories = [
+    { name: "Mobile", href: "#", icon: UserIcon },
+    { name: "Computer", href: "#", icon: ArchiveBoxIcon },
+    { name: "Laptop", href: "#", icon: StarIcon },
+    { name: "Clothes", href: "#", icon: ArrowLeftOnRectangleIcon },
+];
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
 }
+
+
 
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -97,70 +116,12 @@ export default function Header() {
                     </button>
                 </div>
                 <Popover.Group className="hidden lg:flex lg:gap-x-12 items-center">
-                    <Popover className="relative">
-                        <Popover.Button className="flex items-center gap-x-1 text-m font-bold leading-6 text-black-800 mb-1 ml-5">
-                            Home
-                            <ChevronDownIcon
-                                className="h-5 w-5 flex-none text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </Popover.Button>
-
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0 translate-y-1"
-                            enterTo="opacity-100 translate-y-0"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100 translate-y-0"
-                            leaveTo="opacity-0 translate-y-1"
-                        >
-                            <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                                <div className="p-4">
-                                    {products.map((item) => (
-                                        <div
-                                            key={item.name}
-                                            className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                                        >
-                                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                                <item.icon
-                                                    className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                                                    aria-hidden="true"
-                                                />
-                                            </div>
-                                            <div className="flex-auto">
-                                                <a
-                                                    href={item.href}
-                                                    className="block font-semibold text-gray-900"
-                                                >
-                                                    {item.name}
-                                                    <span className="absolute inset-0" />
-                                                </a>
-                                                <p className="mt-1 text-gray-600">
-                                                    {item.description}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                                    {callsToAction.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                                        >
-                                            <item.icon
-                                                className="h-5 w-5 flex-none text-gray-400"
-                                                aria-hidden="true"
-                                            />
-                                            {item.name}
-                                        </a>
-                                    ))}
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </Popover>
+                    <a
+                        href="#"
+                        className="text-m font-bold leading-6 text-black-800 mb-1"
+                    >
+                        Home
+                    </a>
 
                     <a
                         href="#"
@@ -174,12 +135,57 @@ export default function Header() {
                     >
                         About
                     </a>
-                    <a
-                        href="#"
-                        className="text-m font-bold leading-6 text-black-800 mb-1"
-                    >
-                        Sign up
-                    </a>
+                    <Popover.Group className="hidden lg:flex lg:gap-x-12  items-center">
+                        <Popover className="relative">
+                            <Popover.Button className="flex items-center gap-x-1 text-m font-bold leading-6 text-black-800 mb-1">
+                                Products
+                                <ChevronDownIcon
+                                    className="h-5 w-5 flex-none text-gray-400"
+                                    aria-hidden="true"
+                                />
+                            </Popover.Button>
+
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-200"
+                                enterFrom="opacity-0 translate-y-1"
+                                enterTo="opacity-100 translate-y-0"
+                                leave="transition ease-in duration-150"
+                                leaveFrom="opacity-100 translate-y-0"
+                                leaveTo="opacity-0 translate-y-1"
+                            >
+                                <Popover.Panel className="absolute -left-10 top-full z-10 mt-3  overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                                    <div className="p-0">
+                                        {categories.map((item) => (
+                                            <div
+                                                key={item.name}
+                                                className="group relative flex items-center gap-x-6 rounded-lg p-3 text-sm leading-1 hover:bg-gray-50"
+                                            >
+                                                <div className="flex h-1 w-6 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                                    <item.icon
+                                                        className="h-6 w-6 text-gray-600 group-hover:text-indigo-600 "
+                                                        aria-hidden="true"
+                                                    />
+                                                </div>
+                                                <div className="flex-auto">
+                                                    <a
+                                                        href={item.href}
+                                                        className="block font-semibold text-gray-900"
+                                                    >
+                                                        {item.name}
+                                                        <span className="absolute inset-0" />
+                                                    </a>
+                                                    <p className="mt-1 text-gray-600">
+                                                        {item.description}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </Popover>
+                    </Popover.Group>
 
                     <div className="relative">
                         <input
@@ -211,13 +217,57 @@ export default function Header() {
                     >
                         <AiOutlineShoppingCart />
                     </a>
-                    <a
-                        href=""
-                        className="flex font-normal leading-6 text-black-800 mb-1"
-                        style={{ fontSize: "25px" }}
-                    >
-                        <AiOutlineUser />
-                    </a>
+                    <Popover.Group className="hidden lg:flex lg:gap-x-12 items-center">
+                        <Popover className="relative">
+                            <Popover.Button className="flex items-center gap-x-1 text-m font-bold leading-6 text-black-800 mb-1 ml-2">
+                                <AiOutlineUser className="h-6 w-6" />
+                                <ChevronDownIcon
+                                    className="h-5 w-5 flex-none text-gray-400"
+                                    aria-hidden="true"
+                                />
+                            </Popover.Button>
+
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-200"
+                                enterFrom="opacity-0 translate-y-1"
+                                enterTo="opacity-100 translate-y-0"
+                                leave="transition ease-in duration-150"
+                                leaveFrom="opacity-100 translate-y-0"
+                                leaveTo="opacity-0 translate-y-1"
+                            >
+                                <Popover.Panel className="absolute -left-10 top-full z-10 mt-3  overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                                    <div className="p-0">
+                                        {products2.map((item) => (
+                                            <div
+                                                key={item.name}
+                                                className="group relative flex items-center gap-x-6 rounded-lg p-3 text-sm leading-1 hover:bg-gray-50"
+                                            >
+                                                <div className="flex h-1 w-6 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                                    <item.icon
+                                                        className="h-6 w-6 text-gray-600 group-hover:text-indigo-600 "
+                                                        aria-hidden="true"
+                                                    />
+                                                </div>
+                                                <div className="flex-auto">
+                                                    <a
+                                                        href={item.href}
+                                                        className="block font-semibold text-gray-900"
+                                                    >
+                                                        {item.name}
+                                                        <span className="absolute inset-0" />
+                                                    </a>
+                                                    <p className="mt-1 text-gray-600">
+                                                        {item.description}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </Popover>
+                    </Popover.Group>
                 </div>
                 {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <a
@@ -331,8 +381,7 @@ export default function Header() {
                     </div>
                 </Dialog.Panel>
             </Dialog>
-            <hr className="w-full"/>
+            <hr className="w-full" />
         </header>
-        
     );
 }
